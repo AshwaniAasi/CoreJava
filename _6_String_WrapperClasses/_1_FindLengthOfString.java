72926502adf28c6a4fb05172385b56e1130975b5
AshwaniAasi <116952974+AshwaniAasi@users.noreply.github.com>
2022-10-29 17:11:16 +0530

    Add files via upload

---
+++ _6_String_WrapperClasses/_1_FindLengthOfString.java
@@ -0,0 +1,14 @@
+package _6_String_WrapperClasses;
+
+public class _1_FindLengthOfString 
+{
+	public static void main(String[] args) 
+	{
+		String str="Ashwani Kumar Chaurasiya";
+		System.out.println("The total Length (with Spaces) of String are : "+str.length());
+		
+		
+		
+		
+	}
+}
